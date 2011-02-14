88b64d479664bb9f443b4b3afdd0019a0e0f7d76
kudonbec <kudonbec@gmail.com>
2011-02-14 20:36:08 +0900

    recreate attend.

---
+++ attend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attend::Application.config.secret_token = '4294a6e41743b4ba2120eac937fd783643e887a45899942f9f2a2977901c1680b9f08f2eb1de6afb59b93e6e896481bdb9bff1f134f33d8f2eed1f7a7578f5fc'
