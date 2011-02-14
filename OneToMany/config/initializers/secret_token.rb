f27e151d023266997765f514796f78c15e84d7cd
kudonbec <kudonbec@gmail.com>
2011-02-14 22:00:28 +0900

    create OneToMany.

---
+++ OneToMany/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneToMany::Application.config.secret_token = 'c5dc94da1a4036fabd6945219cd978928c124c7cfe39b5dbf4e3eea4ee72b5ea752ec888472a7d6244c9baf2ec30d80aa3dd85a37118e3c288f59a83313cdb11'
