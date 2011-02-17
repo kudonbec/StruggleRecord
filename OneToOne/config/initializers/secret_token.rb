1dd543873bea6016043207fa86823a6c6f09bf28
kudonbec <kudonbec@gmail.com>
2011-02-17 11:06:29 +0900

    create OneToOne.

---
+++ OneToOne/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneToOne::Application.config.secret_token = '76facd59b1ecfe7d067497fc4c60f8894f4b18ae507707a114c9bccedfa406324b8dc4e3a75aadf222ecfb236b4b29676ce80cfd5bd7659586b5cb6897d7da82'
